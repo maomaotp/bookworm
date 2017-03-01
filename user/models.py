0cdedb50e0467d7cecefa700b18d6c2ea8256e75
maomaotp <404709954@qq.com>
2017-03-01 19:40:28 +0800

    new

--- user/models.py
+++ user/models.py
@@ -2,19 +2,22 @@ from django.db import models
 
 class UserInfo(models.Model):
     mobile = models.CharField(max_length=12)
-    nick_name = models.CharField(max_length=50)
+    nick_name = models.CharField(max_length=50, null=True)
     passwd = models.CharField(max_length=20, null=True)
     sex = models.IntegerField(null=True)
     birthday = models.CharField(max_length=20, null=True)
     ctime = models.DateTimeField(auto_now=True)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.nick_name
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
+#class Question(models.Model):
+#    question_text = models.CharField(max_length=200)
+#    pub_date = models.DateTimeField('date published')
+#
+#
+#class Choice(models.Model):
+#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#    choice_text = models.CharField(max_length=200)
+#    votes = models.IntegerField(default=0)
+#
